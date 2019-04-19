eecc57299c943917bd3a2a4bba6515a76940a64c
globalnomad <andrewlfleury@gmail.com>
2019-04-19 16:23:46 +0900

    Prepping refactor

--- scrap.py
+++ scrap.py
@@ -1,5 +1,6 @@
 class track():
     total_tracks = 0
+
     def __init__(self, artist, title, cuetime):
         self.__artist = artist
         self.__title = title
@@ -7,12 +8,32 @@ class track():
         self.__trackNumber = track.total_tracks + 1
         self.__trackID = track.total_tracks + 1
         track.total_tracks += 1
+
     def updateTrackNumber(self, tracklist):
         difference = self.trackNumber - tracklist.index(self)
         if difference != 1:
             self.trackNumber -= difference-1
         else:
             pass
+
+    def display_cue(self):
+        h = str(self.__cuetime.get() // 3600).zfill(2)
+        m = str((self.__cuetime.get() - h * 3600) // 60).zfill(2)
+        s = str(self.__cuetime.get() - h * 3600 - m * 60).zfill(2)
+        return f'{h}:{m}:{s}'
+
+    def clean_cuetime(self, cuetime):
+        if self.trackNumber == '01' and not cuetime:
+            return 0
+        elif ':' not in cuetime:
+            return 0
+        else:
+            hms = cuetime.split(':')
+            if len(hms) < 3:
+                hms.insert(0,'0')
+            [h, m, s] = self.__cuetime.split(':')
+            return int(h) * 3600 + int(m) * 60 + int(s)
+
     def adjust_cuetime(self, min_adj, sec_adj, early_or_late):
         adjustment = int(min_adj) * 60 + int(sec_adj)
         if self.__cuetime:
@@ -27,16 +48,15 @@ class track():
             self.__cuetime =':'.join([str(cue_secs // 60).zfill(2), str(cue_secs % 60).zfill(2), '00'])
         else:
             pass
-    def clean_cuetime(self, cuetime=None):
-        if cuetime:
-            self.__cuetime = cuetime
-        else:
-            if self.trackNumber == '01' and not self.__cuetime:
-                self.__cuetime = '00:00:00'
-            elif self.__cuetime.count(':') == 1:
-                min_sec = self.__cuetime.split(':')
-                self.__cuetime = '{0}:{1}:00'.format(min_sec[0].zfill(2), min_sec[1])
-            else:
-                hms = self.__cuetime.split(':')
-                m, s = int(hms[0]) * 60 + int(hms[1]), hms[2]
-                self.__cuetime = f'{m}:{s}:00'
+
+class cuefile():
+    def __init__(self, artist, title, year, genre, filepath='', website='')
+        self.artist = artist
+        self.title = title
+        self.date = year
+        self.genre = genre
+        self.filepath = filepath
+        self.tracklist = []
+        self.website = website
+    def save(self, website=None, soup=None):
+
